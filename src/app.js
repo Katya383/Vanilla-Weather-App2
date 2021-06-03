4df2cf06579569629e0f5737f6d2226b54db7615
Katya383 <82849906+Katya383@users.noreply.github.com>
2021-06-02 20:38:21 -0400

    updated weather icon

--- src/app.js
+++ src/app.js
@@ -42,16 +42,28 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http:openweathermap.org/img/wn${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setattribure("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
-let apiKey = "ae4032eef8a3f74abc226eb2373f89f0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "ae4032eef8a3f74abc226eb2373f89f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-console.log(apiUrl);
+search("New York");
