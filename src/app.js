91b497488935b46ccacdf9c6d1c2457f7d83573a
Katya383 <82849906+Katya383@users.noreply.github.com>
2021-06-06 18:18:12 -0400

    Added js/html weather forecast

--- src/app.js
+++ src/app.js
@@ -22,6 +22,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Mon", "Tue", "Wed", "Thu"];
+  let forecastHTML = `<dix class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+    <div class="weather-forecast-date">${day}</div>
+    <img
+    src="http://openweathermap.org/img/wn/50d@2x.png"
+    alt=""
+    width="42"
+    />
+    <div class ="weather-forecast-temperatures">
+    <span class="weather-forecast-temperature-max"> 20°</span>
+    <span class ="weather-forecast-temperature-min"> 13°</span>
+    </div>
+    </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
